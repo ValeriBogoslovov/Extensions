0a71421a7ecb784058e342e71a70e3662e7895af
Valer Bogoslovov <valeribogoslovov@hotmail.com>
2018-02-12 16:19:22 +0200

    ApplicationBuilderExtensions
    seeding roles and users on startup

---
+++ ApplicationBuilder/ApplicationBuilderExtensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace ApplicationBuilderExtensions
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using System;
+    using System.Threading.Tasks;
+
+    public static class ApplicationBuilderExtensions
+    {
+        //call this method inside 'Configure' method in 'Startup.cs' --> ASP.NET Core 2.0
+        public static IApplicationBuilder UseDatabaseMigrations(this IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices
+                                         .GetRequiredService<IServiceProvider>()
+                                         .CreateScope())
+            {
+                serviceScope.ServiceProvider.GetService<YourDbContext>
+            }
+
+            return app;
+        }
+    }
+
+    public class YourDbContext : DbContext
+    {
+
+    }
+}
