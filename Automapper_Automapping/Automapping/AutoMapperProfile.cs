078bc811b5622c2f3eeba9d455a687f5890fdd5c
Valer Bogoslovov <valeribogoslovov@hotmail.com>
2018-02-12 16:19:50 +0200

    changed

--- Automapper_Automapping/Automapping/AutoMapperProfile.cs
+++ Automapper_Automapping/Automapping/AutoMapperProfile.cs
@@ -8,21 +8,44 @@
     {
         public AutoMapperProfile()
         {
-            var assemblies = AppDomain
+            var allTypes = AppDomain
                 .CurrentDomain
                 .GetAssemblies()
                 .Where(a => a.GetName().Name.Contains("Automapping"))//name of the project
-                .SelectMany(a => a.GetTypes())
+                .SelectMany(a => a.GetTypes());
+
+            allTypes
                 .Where(t => t.IsClass && !t.IsAbstract && t
                     .GetInterfaces()
                     .Where(i => i.IsGenericType)
                     .Select(i => i.GetGenericTypeDefinition())
-                    .FirstOrDefault() == typeof(IMapFrom<>))
+                    .Contains(typeof(IMapFrom<>)))
                 .Select(t => new
                 {
-
+                    Destination = t,
+                    Source = t
+                        .GetInterfaces()
+                        .Where(i => i.IsGenericType)
+                        .Select(i => new
+                        {
+                            Definition = i.GetGenericTypeDefinition(),
+                            Arguments = i.GetGenericArguments()
+                        })
+                        .Where(i => i.Definition == typeof(IMapFrom<>))
+                        .SelectMany(i => i.Arguments)
+                        .First()
                 })
+                .ToList()
+                .ForEach(mapping => this.CreateMap(mapping.Source, mapping.Destination));
 
+            allTypes
+                .Where(t => t.IsClass
+                    && !t.IsAbstract
+                    && typeof(IHaveCustomMapping).IsAssignableFrom(t))
+                .Select(Activator.CreateInstance)
+                .Cast<IHaveCustomMapping>()
+                .ToList()
+                .ForEach(mapping => mapping.ConfigureMapping(this));
         }
     }
 }
--- Automapper_Automapping/Automapping/IHaveCustomMapping.cs
+++ Automapper_Automapping/Automapping/IHaveCustomMapping.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using AutoMapper;
 
 namespace Automapping
 {
-    interface IHaveCustomMapping
+    public interface IHaveCustomMapping
     {
+        void ConfigureMapping(Profile profile);
     }
 }
